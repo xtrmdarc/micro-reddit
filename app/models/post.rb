3fd84b0ff821dec3523482b6784fa567b8d72ed2
xtrmdarc <diegoreyesco@hotmail.com>
2020-03-20 17:39:09 -0500

    Add post model

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  validates :content, presence: true, length: { minimum: 5 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_210826) do
+ActiveRecord::Schema.define(version: 2020_03_20_221454) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.string "img_url"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
